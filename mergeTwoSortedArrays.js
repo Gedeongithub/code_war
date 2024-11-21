50b9dae56a848e4fbcfea5b10848b5ff872b6fa8
Gedeon DUFITIMANA <gedeon.dufitimana2023@kepler.org>
2024-11-21 20:11:19 +0200

    The block of code to merge the two arrays elements in the same new one array

--- mergeTwoSortedArrays.js
+++ mergeTwoSortedArrays.js
@@ -1,6 +1,6 @@
 
 const mergeArrays = (arr1,arr2) => {
     let arr3 = arr1.concat(arr2);
-    return [...new Set(arr3)].sort()
+  
+    return [...new Set(arr3)].sort((a,b)=> a-b)
 }
-console.log(mergeArrays([2,3,3,1,4],[5,4,3]));
