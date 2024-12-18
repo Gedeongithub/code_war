24d18640c98072c46266eb4d475857e5f01ab40e
Gedeon DUFITIMANA <gedeon.dufitimana2023@kepler.org>
2024-12-18 10:07:58 +0200

    count a number of available sheep in list

---
+++ countingSheep.js
@@ -0,0 +1,9 @@
+function countSheeps(sheep) {
+    let arr =[]
+  for(let i =0; i<sheep.length;i++){
+      if(sheep[i]===true){
+          arr.push(sheep[i])
+      }
+  } 
+  return arr.length;
+}
