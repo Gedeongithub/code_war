15687829ccababf08096fc655c803f8a2af2c179
Gedeon DUFITIMANA <gedeon.dufitimana2023@kepler.org>
2025-01-04 11:38:13 +0200

    Create a rooms

---
+++ rooms.js
@@ -0,0 +1,19 @@
+// Add rooms here
+var rooms = {
+    room1 : {
+    name:"Eating room",
+    description:"this room is used to by people who are eating",
+    completed: false
+  },
+    room2 :{
+        name:'Leaving room',
+        description:'this room is used as a living space',
+        completed: false
+    },
+    room3 :{
+        name:'Bathroom',
+        description:'cleaning and having a bath ',
+        completed: false
+    }
+  
+  }
