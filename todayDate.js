52b5b52837f78008c83a792e54a4e1236698db01
Gedeon DUFITIMANA <gedeon.dufitimana2023@kepler.org>
2025-03-05 09:51:13 +0200

    chec if the given date is the same as today's date

---
+++ todayDate.js
@@ -0,0 +1,8 @@
+const isToday =date => {
+    const today = new Date();
+    return (
+        date.getFullYear() === today.getFullYear() &&
+        date.getMonth() === today.getMonth() &&
+        date.getDate() === today.getDate()
+    );
+}
